7b039f74e769beeed3360e97b1ba447aa0049025
christopherritter <christopherritter@gmail.com>
2016-07-28 09:46:29 -0400

    Disabled deprecated forms

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 import { APP_ROUTES_PROVIDERS } from './app/app.routes';
 import { ShoppingListService } from './app/shopping-list';
@@ -9,5 +10,10 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent, [ShoppingListService, APP_ROUTES_PROVIDERS]);
+bootstrap(AppComponent, [
+  ShoppingListService, 
+  APP_ROUTES_PROVIDERS,
+  disableDeprecatedForms(), // disabled old forms
+  provideForms() // enables new forms
+]);
 
